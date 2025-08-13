b9a986686202a3f2c85e6fa350553615d882c294
whdjh <wngns9807@gmail.com>
2025-08-14 02:35:47 +0900

    chore: gnb 중복 태그 삭제

--- allright_web/src/components/common/Gnb.tsx
+++ allright_web/src/components/common/Gnb.tsx
@@ -11,35 +11,33 @@ export default function Gnb() {
   ];
 
   return (
-    <header className="fixed top-0 left-0 z-50 w-full">
-      <div className="flex h-[3.75rem] w-full items-center justify-center px-[1.5rem]">
-        <div className="flex w-full max-w-[84rem] items-center justify-between text-white mt-[1rem]">
-          <nav className="flex items-center gap-[0.75rem]">
+    <header className="fixed top-0 left-0 z-50 w-full bg-[#151515]">
+      <div className="flex h-[3.75rem] w-full max-w-[84rem] items-center justify-between px-[1.5rem] text-white mt-[1rem]">
+        <nav className="flex items-center gap-[0.75rem]">
+          <Link 
+            href='/'
+            aria-label="메인페이지로 이동"
+          >
+            <Logo width={60} height={60} />
+          </Link>
+          {navItems.map(({ href, label }) => (
             <Link 
-              href='/'
-              aria-label="메인페이지로 이동"
+              key={href}
+              href={href}
+              aria-label={`${label} 페이지로 이동`}
+              className="text-white"
             >
-              <Logo width={60} height={60} />
+              {label}
             </Link>
-            {navItems.map(({ href, label }) => (
-              <Link 
-                key={href}
-                href={href}
-                aria-label={`${label} 페이지로 이동`}
-                className="text-white"
-              >
-                {label}
-              </Link>
-            ))}
-          </nav>
-          <div>
-            <Link
-              href="/login"
-              aria-label="로그인 페이지로 이동"
-            >
-              로그인
-            </Link>
-          </div>
+          ))}
+        </nav>
+        <div>
+          <Link
+            href="/login"
+            aria-label="로그인 페이지로 이동"
+          >
+            로그인
+          </Link>
         </div>
       </div>
     </header>
