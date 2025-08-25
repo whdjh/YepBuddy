bfb577195a82c537e9778f28d519131404a3e727
whdjh <wngns9807@gmail.com>
2025-08-25 17:28:09 +0900

    feat: 카드 컴포넌트 썸네일, 트레이너이름, 헬스장 위치 합성 패턴으로 개발

---
+++ allright_web/src/components/common/Card/TitleBlock.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+interface TitleBlockProps {
+  author: string;
+  location: string; // TODO: 네이버 API 연동하여 실제 근무 헬스장 위치 지정
+  className?: string;
+}
+
+export const TitleBlock = ({ author, location, className = '' }: TitleBlockProps) => {
+  return (
+    <div className={`${className}`}>
+      <p className="text-lg font-semibold mb-2">
+        {author}
+      </p>
+      <p className="text-sm">
+        {location}
+      </p>
+    </div>
+  );
+};
---
+++ allright_web/src/components/common/Card/index.tsx
@@ -0,0 +1,7 @@
+import { TitleBlock } from "./TitleBlock";
+import { Thumbnail } from "./Thumbnail";
+
+export const Card = {
+  TitleBlock,
+  Thumbnail,
+};
