32debb3a2e219b3d81fb55209079eac7b0ddd5c4
whdjh <wngns9807@gmail.com>
2025-08-25 17:47:49 +0900

    feat: 카드 태그

--- allright_web/src/components/common/Card/index.tsx
+++ allright_web/src/components/common/Card/index.tsx
@@ -1,7 +1,9 @@
 import { TitleBlock } from "./TitleBlock";
 import { Thumbnail } from "./Thumbnail";
+import { TagList } from "./TagList";
 
 export const Card = {
   TitleBlock,
   Thumbnail,
+  TagList,
 };
