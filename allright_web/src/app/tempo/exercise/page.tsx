14e269ec2fb8fb32165ddcf895762effc1d47b1d
whdjh <wngns9807@gmail.com>
2025-08-28 17:17:05 +0900

    feat: tempo/exercise 페이지 UI구성 및 타이머

--- allright_web/src/app/tempo/exercise/page.tsx
+++ allright_web/src/app/tempo/exercise/page.tsx
@@ -1,19 +1,77 @@
 'use client';
 
 import { useTempoStore } from "@/stores/useTempo";
+import { useState, useEffect } from "react";
 
-export default function Exercise() {
+export default function ExerciseUI() {
+  // selcted, sets, rests, name, conce~, e~, reps
   const { selected, tempoFormValues } = useTempoStore();
 
+  const [remainingRestTime, setRemainingRestTime] = useState<number | null>(Number(tempoFormValues.rests));
+  const [currentSet, setCurrentSet] = useState(1);
+
+  useEffect(() => {
+    if (remainingRestTime === null) {
+      return;
+    }
+  
+    // 0이하면 currentSet이 1증가
+    if (remainingRestTime <= 0) {
+      // 세트 증가
+      setCurrentSet((prev) => prev + 1);
+
+      // 세트가 끝났으면 휴식시간 초기화
+      if (currentSet < Number(tempoFormValues.sets)) {
+        setRemainingRestTime(Number(tempoFormValues.rests));
+      }
+      // TODO: 모든 세트 끝나면 모달이 띄어지고 운동 추가 or 종료 의사를 물음
+      // TODO: 운동 추가시 tempo페이지로 이동
+      // TODO: 운동 종료시 dairy페이지로 이동
+      else {
+        setRemainingRestTime(null);
+      }
+      return; 
+    }
+
+    const timer = setInterval(() => {
+      setRemainingRestTime((prev) => (prev !== null ? prev - 1 : null));
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, [remainingRestTime]);
+
+
   return (
-    <div className="p-5 flex flex-col gap-3">
-      <h2 className="text-xl font-bold">운동 정보</h2>
-      <p>시작 종류: {selected === 'concentric' ? '수축 먼저' : '이완 먼저'}</p>
-      <p>수축 시간: {tempoFormValues.eccentric}초</p>
-      <p>이완 시간: {tempoFormValues.concentric}초</p>
-      <p>운동 횟수: {tempoFormValues.reps}회</p>
-      <p>세트 수: {tempoFormValues.sets}세트</p>
-      <p>휴식 시간: {tempoFormValues.rests}초</p>
+    <div className="flex flex-col p-6 gap-10">
+      <div className="bg-emerald-600/80 p-6 rounded-2xl">
+        <p className="text-white text-xl font-semibold text-center">
+          현재 운동 종목
+        </p>
+        <p className="text-white text-3xl font-bold text-center">
+          {tempoFormValues.name}
+        </p>
+      </div>
+      {/* 남은 세트수 */}
+      <div className="bg-emerald-600/80 p-6 rounded-2xl">
+        <p className="text-white text-xl font-semibold text-center">
+          남은 세트수
+        </p>
+        <p className="text-white text-3xl font-bold text-center">
+          {Number(tempoFormValues.sets) - currentSet}
+        </p>
+      </div>
+
+      {/* 남은 휴식시간 */}
+      {remainingRestTime !== null && (
+        <div className="bg-emerald-600/80 p-6 rounded-2xl">
+          <p className="text-white text-xl font-semibold text-center">
+            남은 휴식시간
+          </p>
+          <p className="text-white text-3xl font-bold text-center">
+            {remainingRestTime}초
+          </p>
+        </div>
+      )}
     </div>
   );
 }
--- allright_web/src/components/product/tempo/InputSection.tsx
+++ allright_web/src/components/product/tempo/InputSection.tsx
@@ -19,7 +19,7 @@ export default function InputSection({ methods, setFormValue }: InputSectionProp
         <Input
           name="eccentric"
           label="수축(초)"
-          placeholder='숫자만 입력하세요.'
+          placeholder='숫자만 입력'
           width='w-[10rem]'
           type="number"
           inputMode="numeric"
@@ -36,7 +36,7 @@ export default function InputSection({ methods, setFormValue }: InputSectionProp
         <Input
           name="concentric"
           label="이완(초)"
-          placeholder='숫자만 입력하세요.'
+          placeholder='숫자만 입력'
           width='w-[10rem]'
           type="number"
           inputMode="numeric"
@@ -55,7 +55,7 @@ export default function InputSection({ methods, setFormValue }: InputSectionProp
         <Input
           name="reps"
           label="운동 횟수(reps)"
-          placeholder='숫자만 입력하세요.'
+          placeholder='숫자만 입력'
           width='w-[10rem]'
           type="number"
           inputMode="numeric"
@@ -72,7 +72,7 @@ export default function InputSection({ methods, setFormValue }: InputSectionProp
         <Input
           name="sets"
           label="세트수(set)"
-          placeholder='숫자만 입력하세요.'
+          placeholder='숫자만 입력'
           width='w-[10rem]'
           type="number"
           inputMode="numeric"
@@ -87,23 +87,44 @@ export default function InputSection({ methods, setFormValue }: InputSectionProp
           }}
         />
       </div>
-      <Input
-        name="rests"
-        label="휴식 시간(초)"
-        placeholder='숫자만 입력하세요.'
-        width='w-[21rem]'
-        type="number"
-        inputMode="numeric"
-        onChange={(e) => handleInputChange("rests", e.target.value)}
-        rules={{
-          required: '필수 입력입니다.',
-          pattern: {
-            value: /^[0-9]+$/,
-            message: '숫자만 입력 가능합니다.',
-          },
-          validate: value => Number(value) <= 600 || '600까지 입력 가능합니다.'
-        }}
-      />
+      <div className='flex justify-start gap-3'>
+        <Input
+          name="rests"
+          label="휴식 시간(초)"
+          placeholder='숫자만 입력'
+          width='w-[10rem]'
+          type="number"
+          inputMode="numeric"
+          onChange={(e) => handleInputChange("rests", e.target.value)}
+          rules={{
+            required: '필수 입력입니다.',
+            pattern: {
+              value: /^[0-9]+$/,
+              message: '숫자만 입력 가능합니다.',
+            },
+            validate: value => Number(value) <= 600 || '600까지 입력 가능합니다.'
+          }}
+        />
+        <Input
+          name="name"
+          label="운동 이름"
+          placeholder='영어, 한글로 입력'
+          width='w-[10rem]'
+          type="text"
+          onChange={(e) => handleInputChange("name", e.target.value)}
+          rules={{
+            required: "필수 입력입니다.",
+            pattern: {
+              value: /^[A-Za-z가-힣\s]+$/,
+              message: "영어와 한글만 입력 가능합니다.",
+            },
+            maxLength: {
+              value: 30,
+              message: "30자 이내로 입력해주세요.",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 }
