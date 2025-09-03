48356862249714ce30c0e1379d41933c407ef66c
whdjh <wngns9807@gmail.com>
2025-09-03 17:30:00 +0900

    feat: 트레이너 페이지 추가

---
+++ allright_web/src/app/trainer/page.tsx
@@ -0,0 +1,9 @@
+import SearchSection from "@/components/product/main/SearchSection";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col gap-3">
+      <SearchSection />
+    </main>
+  );
+}
