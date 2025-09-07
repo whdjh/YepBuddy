20aa7749f0dd0eecda2756681f66814ec459049a
whdjh <wngns9807@gmail.com>
2025-09-07 20:24:49 +0900

    chore: 개행수정

--- allright_web/src/app/trainer/page.tsx
+++ allright_web/src/app/trainer/page.tsx
@@ -5,10 +5,7 @@ import { mockProductCards } from "@/mock/cardData";
 export default function Trainer() {
   return (
     <div className="space-y-20">
-      <Hero
-        title="트레이너"
-        subtitle="가장 인기 있는 트레이너"
-      />
+      <Hero title="트레이너" subtitle="가장 인기 있는 트레이너" />
       <CardSection cards={mockProductCards} />
     </div>
   );
