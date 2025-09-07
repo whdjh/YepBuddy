1241415170049b3880caddb5199096045aeb5d6c
whdjh <wngns9807@gmail.com>
2025-09-08 00:23:38 +0900

    chore: submit으로 함수명 변경

--- allright_web/src/app/trainer/submit/page.tsx
+++ allright_web/src/app/trainer/submit/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "@/components/common/hero";
 import TrainerRegistrationForm from "@/components/product/trainer/submit/TrainerRegistrationForm";
 
-export default function Page() {
+export default function Submit() {
   return (
     <div className="space-y-10 flex flex-col justify-center">
       <Hero title="트레이너 등록" subtitle="모든 사람이 볼 수 있게 당신을 알리세요!" />
