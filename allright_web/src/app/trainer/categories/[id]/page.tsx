bd85ef3776e30248374e861d5cbfef394c6e21e5
whdjh <wngns9807@gmail.com>
2025-09-07 21:06:35 +0900

    feat: submit 페이지 라우팅 구성

--- allright_web/src/app/trainer/categories/[id]/page.tsx
+++ allright_web/src/app/trainer/categories/[id]/page.tsx
@@ -2,7 +2,7 @@ import { Hero } from "@/components/common/hero";
 import CardSection from "@/components/product/trainer/CardSection";
 import { mockProductCards } from "@/mock/cardData";
 
-export default function Categories() {
+export default function CategoriesId() {
   // MEMO: 카테고리별 ID([id])값을 가져와서 해당 ID에 해당하는 카드만 보여주기
   return (
     <div className="space-y-20">
---
+++ allright_web/src/app/trainer/submit/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "@/components/common/hero";
+
+export default function Submit() {
+  return (
+    <div className="space-y-10">
+      <Hero title="트레이너 등록" subtitle="모든 사람이 볼 수 있게 당신을 알리세요!" />
+    </div>
+  );
+}
