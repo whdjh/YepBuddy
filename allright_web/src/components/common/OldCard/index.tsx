dd495274e9d3a35106ac5a71b342d4af0d351bb0
whdjh <99352927+whdjh@users.noreply.github.com>
2025-09-14 19:05:26 +0900

    del: oldcard

--- allright_web/src/components/common/OldCard/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TitleBlock } from "./TitleBlock";
-import { Thumbnail } from "./Thumbnail";
-import { TagList } from "./TagList";
-
-export const Card = {
-  TitleBlock,
-  Thumbnail,
-  TagList,
-};
