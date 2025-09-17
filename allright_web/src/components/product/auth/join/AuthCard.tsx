7d51e892ad6b4e4db7cb73c80450b0e9a577263c
whdjh <wngns9807@gmail.com>
2025-09-17 20:46:10 +0900

    chore: sign 페이지 ui 수정

--- allright_web/src/components/product/auth/join/AuthCard.tsx
+++ allright_web/src/components/product/auth/join/AuthCard.tsx
@@ -3,8 +3,10 @@
 import { useForm, FormProvider } from "react-hook-form";
 import { Button } from "@/components/ui/button";
 import InputPair from "@/components/common/InputPair";
+import { MessageCircleIcon } from "lucide-react";
+import Link from "next/link";
 
-type JoinForm = { username: string;  email: string; password: string };
+type JoinForm = { username: string; email: string; password: string };
 
 export default function AuthCard() {
   const methods = useForm<JoinForm>({
@@ -20,51 +22,71 @@ export default function AuthCard() {
   };
 
   return (
-    <FormProvider {...methods}>
-      <div className="flex flex-col relative items-center justify-center h-full">
-        <div className="flex items-center flex-col justify-center w-full max-w-md gap-10">
-          <h1 className="text-2xl font-semibold">회원가입</h1>
-          <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)} noValidate>
-            <InputPair
-              name="username"
-              id="username"
-              label="이름"
-              description="이름을 입력하세요."
-              placeholder="이주훈"
-              rules={{
-                required: "필수 입력 사항입니다.",
-              }}
-            />
-            <InputPair
-              name="email"
-              id="email"
-              label="이메일"
-              description="이메일을 입력하세요."
-              placeholder="allright@allright.com"
-              rules={{
-                required: "필수 입력 사항입니다.",
-                pattern: {
-                  value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-                  message: "이메일 형태가 아닙니다.",
-                },
-              }}
-            />
-            <InputPair
-              name="password"
-              id="password"
-              label="비밀번호"
-              description="비밀번호를 입력하세요."
-              type="password"
-              placeholder="••••••••"
-              rules={{ required: "필수 입력 사항입니다." }}
-            />
+    <div className="w-full max-w-md">
+      <FormProvider {...methods}>
+        <div className="flex flex-col relative items-center justify-center h-full">
+          <div className="flex items-center flex-col justify-center w-full max-w-md gap-10">
+            <h1 className="text-2xl font-semibold">회원가입</h1>
+            <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)} noValidate>
+              <InputPair
+                name="username"
+                id="username"
+                label="이름"
+                description="이름을 입력하세요."
+                placeholder="이주훈"
+                rules={{
+                  required: "필수 입력 사항입니다.",
+                }}
+              />
+              <InputPair
+                name="email"
+                id="email"
+                label="이메일"
+                description="이메일을 입력하세요."
+                placeholder="allright@allright.com"
+                rules={{
+                  required: "필수 입력 사항입니다.",
+                  pattern: {
+                    value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                    message: "이메일 형태가 아닙니다.",
+                  },
+                }}
+              />
+              <InputPair
+                name="password"
+                id="password"
+                label="비밀번호"
+                description="비밀번호를 입력하세요."
+                type="password"
+                placeholder="••••••••"
+                rules={{ required: "필수 입력 사항입니다." }}
+              />
 
-            <Button className="w-full" type="submit" disabled={isSubmitting}>
-              {isSubmitting ? "회웝가입 중" : "회원가입"}
-            </Button>
-          </form>
+              <Button className="w-full" type="submit" disabled={isSubmitting}>
+                {isSubmitting ? "회웝가입 중" : "회원가입"}
+              </Button>
+            </form>
+          </div>
+        </div>
+      </FormProvider>
+      <div className="w-full flex flex-col items-center gap-8">
+        <div className="w-full flex flex-col items-center gap-2">
+          <hr className="w-full border-white/10" />
+          <span className="text-xs text-muted-foreground uppercase font-medium">
+            소셜 회원가입
+          </span>
+          <hr className="w-full border-white/10" />
+
+        </div>
+        <div className="w-full flex flex-col gap-2">
+          <Button variant="outline" className="w-full" asChild>
+            <Link href="/auth/social/kakao/start">
+              <MessageCircleIcon className="w-4 h-4" />
+              카카오톡
+            </Link>
+          </Button>
         </div>
       </div>
-    </FormProvider>
+    </div>
   );
 }
