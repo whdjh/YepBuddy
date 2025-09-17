e4e8d84ad59d10385ab9780a9c50f6b845b30c55
whdjh <wngns9807@gmail.com>
2025-09-17 21:16:42 +0900

    chore: constants 한글로 수정

--- allright_web/src/constants/community.ts
+++ allright_web/src/constants/community.ts
@@ -1,8 +1,8 @@
-export const SORT_OPTIONS = ["newest", "popular"] as const;
+export const SORT_OPTIONS = ["최신순", "인기순"] as const;
 export const PERIOD_OPTIONS = [
-  "all",
-  "today",
-  "week",
-  "month",
-  "year",
+  "모든글",
+  "오늘",
+  "이번주",
+  "이번달",
+  "올해",
 ] as const;
