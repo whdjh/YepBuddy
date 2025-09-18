00a27ff909e479f7974acf431eadda01ba7c8061
whdjh <wngns9807@gmail.com>
2025-09-19 01:33:13 +0900

    feat: message페이지

--- allright_web/src/app/my/messages/page.tsx
+++ allright_web/src/app/my/messages/page.tsx
@@ -6,13 +6,14 @@ import {
   SidebarProvider,
 } from "@/components/ui/sidebar";
 import { MessageCard } from "@/components/common/Card/MessageCard";
+import { MessageCircleIcon } from "lucide-react";
 
 export default function Message() {
   return (
-    <div className="">
-      <SidebarProvider className="flex max-h-[calc(100vh-14rem)] overflow-hidden h-[calc(100vh-14rem)] min-h-full">
-        <Sidebar className="pt-16" variant="floating">
-          <SidebarContent>
+    <div className="h-[calc(100vh-14rem)] overflow-hidden">
+      <SidebarProvider className="flex h-full">
+        <Sidebar className="" variant="floating">
+          <SidebarContent className="overflow-y-auto">
             <SidebarGroup>
               <SidebarMenu>
                 {Array.from({ length: 20 }).map((_, index) => (
@@ -28,6 +29,12 @@ export default function Message() {
             </SidebarGroup>
           </SidebarContent>
         </Sidebar>
+        <div className="flex flex-col gap-5 items-center justify-center flex-1">
+          <MessageCircleIcon className="size-12 text-muted-foreground" />
+          <h1 className="text-xl text-muted-foreground font-semibold">
+            Click on a message in the sidebar to view it.
+          </h1>
+        </div>
       </SidebarProvider>
     </div>
   );
