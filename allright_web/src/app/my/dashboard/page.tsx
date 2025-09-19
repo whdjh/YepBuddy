c5b9ce7d8d7411367bd6b78869fc3eb44200cee7
whdjh <wngns9807@gmail.com>
2025-09-20 02:13:20 +0900

    feat: profilecharts 컴포넌트

--- allright_web/src/app/my/dashboard/page.tsx
+++ allright_web/src/app/my/dashboard/page.tsx
@@ -1,12 +1,13 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 import SideInventory from "@/components/product/my/dashboard/SideInventory";
+import ProfileChart from "@/components/product/my/dashboard/ProfileChart";
 
 export default function Dashboard() {
   return (
     <SidebarProvider className="flex h-full">
       <SideInventory />
-      <div>
-        차트가 들어갈 자리임
+      <div className="w-full h-full">
+        <ProfileChart />
       </div>
     </SidebarProvider>
   );
---
+++ allright_web/src/components/product/my/dashboard/ProfileChart.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { Line, CartesianGrid, LineChart, XAxis } from "recharts";
+import { ChartContainer, ChartTooltipContent, ChartTooltip } from "@/components/ui/chart";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+];
+const chartConfig = {
+  views: {
+    label: "👁️",
+    color: "#16a34a",
+  },
+};
+
+export default function ProfileChart() {
+  return (
+    <Card className="w-1/2 border border-white/10">
+      <CardHeader>
+        <CardTitle>프로필 방문자 수</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            accessibilityLayer
+            data={chartData}
+            margin={{
+              left: 12,
+              right: 12,
+            }}
+          >
+            <CartesianGrid vertical={false} />
+            <XAxis
+              dataKey="month"
+              tickLine={false}
+              axisLine={false}
+              tickMargin={8}
+              tickFormatter={(value) => value.slice(0, 3)}
+            />
+            <Line
+              dataKey="views"
+              type="natural"
+              stroke="var(--color-views)"
+              strokeWidth={2}
+              dot={false}
+            />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent hideLabel />}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
