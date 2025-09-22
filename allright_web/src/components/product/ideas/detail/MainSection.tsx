4f2d022078e69401fb4d45ab8593df9ddf2392ba
whdjh <wngns9807@gmail.com>
2025-09-23 01:08:54 +0900

    feat: ideas detail

--- allright_web/src/components/product/ideas/detail/MainSection.tsx
+++ allright_web/src/components/product/ideas/detail/MainSection.tsx
@@ -15,7 +15,7 @@ export default function MainSection() {
         <DotIcon className="w-4 h-4" />
         <span>12 hours ago</span>
         <DotIcon className="w-4 h-4" />
-        <Button variant="outline">
+        <Button variant="outline" className="border border-white/10">
           <HeartIcon className="w-4 h-4" />
           <span>12</span>
         </Button>
