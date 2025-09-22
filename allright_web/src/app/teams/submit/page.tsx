01046f31447b6ef352c30c68720ed67f0852299a
whdjh <wngns9807@gmail.com>
2025-09-23 01:16:14 +0900

    feat: team 제출페이지

--- allright_web/src/app/teams/submit/page.tsx
+++ allright_web/src/app/teams/submit/page.tsx
@@ -3,7 +3,7 @@ import FormSection from "@/components/product/teams/submit/FormSection";
 
 export default function TeamsSubmit() {
   return (
-    <div className="space-y-20">
+    <div className="space-y-20 p-2 tab:p-5">
       <Hero title="회원모집 등록" subtitle="피티공고를 등록해보세요!" />
       <FormSection />
     </div>
