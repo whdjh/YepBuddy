106235d32883458fd7bffa1501ec43917dfdf26b
whdjh <wngns9807@gmail.com>
2025-09-22 23:04:53 +0900

    fix: 가상화 break-point 수정

--- allright_web/src/components/common/VirtuoInfinityScroll.tsx
+++ allright_web/src/components/common/VirtuoInfinityScroll.tsx
@@ -62,7 +62,7 @@ export default function VirtuoInfinityScroll<T>({
               ref={ref}
               className={
                 layout === 'grid'
-                  ? 'grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'
+                  ? 'grid grid-cols-1 tab:grid-cols-2 pc:grid-cols-3 gap-4'
                   : 'flex flex-col gap-4'
               }
             />
