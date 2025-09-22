3dcc8ab61f568e1fd6aad157ac2616b1e92297dc
whdjh <wngns9807@gmail.com>
2025-09-22 23:05:06 +0900

    fix: 트레이너 페이지 패딩값 수정

--- allright_web/src/app/trainer/page.tsx
+++ allright_web/src/app/trainer/page.tsx
@@ -4,7 +4,7 @@ import { mockProductCards } from "@/mock/productCardData";
 
 export default function Trainer() {
   return (
-    <div className="space-y-20 p-5">
+    <div className="space-y-20 tab:p-5 p-0">
       <Hero title="트레이너" subtitle="가장 인기 있는 트레이너" />
       <CardSection cards={mockProductCards} />
     </div>
