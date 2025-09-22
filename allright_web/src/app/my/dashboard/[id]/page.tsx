c997488378ebec12984e43be5c901960c26582da
whdjh <wngns9807@gmail.com>
2025-09-23 01:39:30 +0900

    feat: detail페이지 사이드바 오픈 버튼

--- allright_web/src/app/my/dashboard/[id]/page.tsx
+++ allright_web/src/app/my/dashboard/[id]/page.tsx
@@ -1,10 +1,13 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import SideInventory from "@/components/product/my/dashboard/SideInventory";
 import ProfileChart from "@/components/product/my/dashboard/ProfileChart";
 
 export default function DashboardDetail() {
   return (
     <SidebarProvider className="flex h-full">
+      <div className="tab:hidden mb-2">
+        <SidebarTrigger />
+      </div>
       <SideInventory />
       <div className="w-full h-full">
         <ProfileChart />
--- allright_web/src/app/my/dashboard/ideas/page.tsx
+++ allright_web/src/app/my/dashboard/ideas/page.tsx
@@ -1,4 +1,4 @@
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import SideInventory from "@/components/product/my/dashboard/SideInventory";
 import CardSection from "@/components/product/ideas/CardSection";
 import { mockIdeasCards } from "@/mock/ideasCardData";
@@ -6,6 +6,9 @@ import { mockIdeasCards } from "@/mock/ideasCardData";
 export default function IdeasDashboard() {
   return (
     <SidebarProvider className="flex min-h-screen">
+      <div className="tab:hidden mb-2">
+        <SidebarTrigger />
+      </div>
       <SideInventory />
       <div className="flex-1">
         <h1 className="text-2xl font-semibold mb-6">내가 구매한 루틴</h1>
