2256b5992627f7f8364ed76a2f064f90553e72ff
whdjh <wngns9807@gmail.com>
2025-09-23 00:59:44 +0900

    feat: detail페이지

--- allright_web/src/app/community/[id]/page.tsx
+++ allright_web/src/app/community/[id]/page.tsx
@@ -7,22 +7,27 @@ import ProfileCard from "@/components/product/community/detail/ProfileCard";
 
 export default function PostPage() {
   return (
-    <div className="space-y-10 p-5">
+    <div className="space-y-10 p-2 tab:p-5">
       <HeaderSection />
-      <div className="grid grid-cols-6 gap-40 items-start">
-        <div className="col-span-4 space-y-10">
-          <div className="flex w-full items-start gap-10">
-            <Button variant="outline" className="flex flex-col h-14">
+
+      <div className="grid grid-cols-1 tab:grid-cols-6 gap-10 tab:gap-40 items-start">
+        <div className="col-span-full tab:col-span-4 space-y-10">
+          <div className="flex w-full items-start gap-6 tab:gap-10">
+            <Button variant="outline" className="flex flex-col h-14 border border-white/10">
               <ChevronUpIcon className="size-4 shrink-0" />
               <span>10</span>
             </Button>
-            <div className="space-y-20">
+            <div className="tab:space-y-20 space-y-5">
               <MainSection />
+              <hr className="w-full border border-white/10"/>
               <CommentSection />
             </div>
           </div>
         </div>
-        <ProfileCard />
+
+        <div className="hidden tab:block tab:col-span-2">
+          <ProfileCard />
+        </div>
       </div>
     </div>
   );
