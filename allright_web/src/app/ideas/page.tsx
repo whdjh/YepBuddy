7076c002e04a86d0705905963d81be5c297d677c
whdjh <wngns9807@gmail.com>
2025-09-23 15:31:31 +0900

    fix: idea 페이지 최종 색상 수정

--- allright_web/src/app/ideas/page.tsx
+++ allright_web/src/app/ideas/page.tsx
@@ -4,7 +4,7 @@ import { mockIdeasCards } from "@/mock/ideasCardData";
 
 export default function Ideas() {
   return (
-    <div className="space-y-20">
+    <div className="p-2 tab:p-5 space-y-5 tab:space-y-20">
       <Hero title="IdeasRoutine" subtitle="당신의 운동 루틴을 찾아보세요!" />
       <CardSection cards={mockIdeasCards} />
     </div>
