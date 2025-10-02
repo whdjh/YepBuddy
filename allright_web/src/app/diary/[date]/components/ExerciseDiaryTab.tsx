815033be333e675e51c90df4a2ec508ea49beaa2
whdjh <wngns9807@gmail.com>
2025-10-02 21:37:11 +0900

    chore: import 없는거 제거

--- allright_web/src/app/diary/[date]/components/ExerciseDiaryTab.tsx
+++ allright_web/src/app/diary/[date]/components/ExerciseDiaryTab.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import BodyPartsSelector from './ExerciseDiaryTab/BodyPartsSelector';
-import ExerciseControls from './ExerciseDiaryTab/ExerciseControls';
 import ExerciseTable from './ExerciseDiaryTab/ExerciseTable';
 import { Exercise, ExerciseData } from '@/types/Diary';
 
