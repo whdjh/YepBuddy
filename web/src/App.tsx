452e9424bb5202dad32f104649ac5e9c296fcc01
whdjh <wngns9807@gmail.com>
2026-08-04 16:36:37 +0900

    feat(web): 다국어 기능 추가

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,28 @@
 import { HomePage } from "./pages/HomePage"
 import { PrivacyPage } from "./pages/PrivacyPage"
 import { SupportPage } from "./pages/SupportPage"
-
-const supportEmail = "wngns9807@gmail.com"
+import { parseLocaleAndRoute, switchLocalePath, texts } from "./i18n"
 
 function App() {
-  return window.location.pathname === "/support" ? (
-    <SupportPage supportEmail={supportEmail} />
-  ) : window.location.pathname === "/privacy" ? (
-    <PrivacyPage supportEmail={supportEmail} />
-  ) : (
-    <HomePage />
-  )
+  const { locale, route } = parseLocaleAndRoute(window.location.pathname)
+  const t = texts[locale]
+  const switchPath = switchLocalePath(locale, route)
+
+  const common = {
+    locale,
+    switchTo: switchPath,
+    text: t,
+  }
+
+  if (route === "/support") {
+    return <SupportPage {...common} />
+  }
+
+  if (route === "/privacy") {
+    return <PrivacyPage {...common} />
+  }
+
+  return <HomePage {...common} />
 }
 
 export default App
